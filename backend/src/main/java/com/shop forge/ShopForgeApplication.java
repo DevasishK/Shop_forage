9a12b5d2f168145c08388f567e013da68807cd90
DevasishK <142006104+DevasishK@users.noreply.github.com>
2026-04-05 23:25:03 +0530

    Add files via upload

---
+++ backend/src/main/java/com/shop forge/ShopForgeApplication.java
@@ -0,0 +1,11 @@
+package com.shopforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopForgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopForgeApplication.class, args);
+    }
+}
